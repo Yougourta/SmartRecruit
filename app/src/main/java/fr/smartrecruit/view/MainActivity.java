5646667aa1c62e2f73a7686257b922d006321717
Yougourta <dy_ait_saada@esi.dz>
2019-01-15 11:55:22 +0100

    add list offers

--- app/src/main/java/fr/smartrecruit/view/MainActivity.java
+++ app/src/main/java/fr/smartrecruit/view/MainActivity.java
@@ -1,11 +1,14 @@
-package fr.smartrecruit;
+package fr.smartrecruit.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import fr.smartrecruit.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    public void loginCandidat(View v){
+        Intent intentLogin = new Intent(this, OffersActivity.class);
+        startActivity(intentLogin);
+    }
 }
