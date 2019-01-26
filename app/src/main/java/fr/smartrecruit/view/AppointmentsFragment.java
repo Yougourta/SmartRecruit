4e20e3ba1a36a960ed6ab4a11bc8bdb1bb61d97f
yanisnait <yanis.nch@gmail.com>
2019-01-26 13:47:56 +0100

    add Fragment Appointments

--- app/src/main/java/fr/smartrecruit/view/AppointmentsFragment.java
+++ app/src/main/java/fr/smartrecruit/view/AppointmentsFragment.java
@@ -21,7 +21,7 @@ public class AppointmentsFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+ 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
