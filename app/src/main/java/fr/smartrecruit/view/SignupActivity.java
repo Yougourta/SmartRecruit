54b8f401b7318994b2032b0a1570279bfbf64514
yanisnait <yanis.nch@gmail.com>
2019-01-26 13:50:59 +0100

    add Sign in

--- app/src/main/java/fr/smartrecruit/view/SignupActivity.java
+++ app/src/main/java/fr/smartrecruit/view/SignupActivity.java
@@ -22,7 +22,9 @@ public class SignupActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+
+
+               Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
