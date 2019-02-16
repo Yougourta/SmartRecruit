6d6292528d19fcaa68acd134a33de834dde43444
Yougourta <dy_ait_saada@esi.dz>
2019-02-16 19:47:45 +0100

    add smartrecruit api access

---
+++ app/src/main/java/fr/smartrecruit/api/SmarRecruitApi.java
@@ -0,0 +1,72 @@
+package fr.smartrecruit.api;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.smartrecruit.data.JobOffer;
+
+public class SmarRecruitApi {
+
+    private final String SERVER_URL = "http://127.0.0.1:5000";
+
+    private Context context;
+    private List<JobOffer> offers;
+
+    public SmarRecruitApi(Context context){
+        this.context = context;
+    }
+
+    public void requestOffers(){
+        offers = new ArrayList();
+        RequestQueue queue = Volley.newRequestQueue(context);
+        final String url = SERVER_URL+"/offers";
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                JsonParser parser = new JsonParser();
+                JsonArray resultsJsonObject = parser.parse(s).getAsJsonObject().get("job-offers").getAsJsonArray();
+                for (int i=0; i<resultsJsonObject.size(); i++){
+                    JsonObject jsonObject = resultsJsonObject.get(i).getAsJsonObject();
+                    JobOffer jobOffer = getJobOffer(jsonObject);
+                    offers.add(jobOffer);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(context, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(request);
+    }
+
+    public List<JobOffer> getOffers(){
+        return this.offers;
+    }
+
+    public JobOffer getJobOffer(JsonObject jsonObject){
+        JobOffer jobOffer = new JobOffer();
+        jobOffer.setId(jsonObject.get("id").getAsString());
+        jobOffer.setPosition(jsonObject.get("position").getAsString());
+        jobOffer.setCompany(jsonObject.get("company").getAsString());
+        jobOffer.setLocation(jsonObject.get("company").getAsString());
+        jobOffer.setDatePosted(jsonObject.get("company").getAsString());
+        jobOffer.setContract(jsonObject.get("contract").getAsString());
+        jobOffer.setDescription(jsonObject.get("description").getAsString());
+        jobOffer.setImg(jsonObject.get("img").getAsString());
+        return jobOffer;
+    }
+}
