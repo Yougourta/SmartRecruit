ecb59570a059c0a80e812379ae10830b6f0d9a41
karim <karim.fatimazahrae@gmail.com>
2019-03-05 09:03:15 +0100

    Merge branch 'master' of C:\Users\Utilisateur\Documents\SmartRecruit with conflicts.

---
+++ app/src/main/java/fr/smartrecruit/view/fragments/Add_offer.java
@@ -0,0 +1,9 @@
+package fr.smartrecruit.view.fragments.recruteur;
+
+import android.support.v4.app.Fragment;
+
+public class Add_offer extends Fragment {
+
+
+    
+}
--- app/src/main/java/fr/smartrecruit/view/fragments/TipsFragment.java
+++ app/src/main/java/fr/smartrecruit/view/fragments/TipsFragment.java
@@ -1,12 +1,17 @@
 package fr.smartrecruit.view.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import fr.smartrecruit.R;
+import fr.smartrecruit.view.activities.CandidatActivity;
+import fr.smartrecruit.view.activities.MainActivity;
+import fr.smartrecruit.view.activities.RecruteurActivity;
 
 public class TipsFragment extends Fragment {
 
@@ -16,4 +21,14 @@ public class TipsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tips, container, false);
     }
+
+    public void onCreate(Bundle savedInstanceState) //A la   creation de la vue
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_formulaire_add_offre); //Afficher la vue portant le nom "jeu"
+    }
+
+    private void setContentView(int fragment_formulaire_add_offre) {
+    }
+
 }
