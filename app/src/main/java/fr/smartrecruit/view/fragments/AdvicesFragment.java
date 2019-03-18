f743d8a3794db6e2e1f298415b39a5eacac44b3f
ilyesad <ilyes92700@gmail.com>
2019-03-18 12:06:20 +0100

    Add list Application Fragment

--- app/src/main/java/fr/smartrecruit/view/fragments/AdvicesFragment.java
+++ app/src/main/java/fr/smartrecruit/view/fragments/AdvicesFragment.java
@@ -1,4 +1,22 @@
 package fr.smartrecruit.view.fragments;
 
+import android.os.Bundle;
+import android.widget.ListView;
+
+import fr.smartrecruit.R;
+
 public class AdvicesFragment {
+    ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
+    }
+
+    private void setContentView(int activity_main) {
+    }
+}
 }
